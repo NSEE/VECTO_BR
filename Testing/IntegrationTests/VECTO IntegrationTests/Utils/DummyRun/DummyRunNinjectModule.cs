d359acae225ce9bc31c4fd1b854ed3a233ccd2e3
Stefanos Doumpoulakis <dubulak@gmail.com>
2025-11-19 11:46:18 +0100

    fix: added missing function to DummyRunNinjectModule

--- Testing/IntegrationTests/VECTO IntegrationTests/Utils/DummyRun/DummyRunNinjectModule.cs
+++ Testing/IntegrationTests/VECTO IntegrationTests/Utils/DummyRun/DummyRunNinjectModule.cs
@@ -15,6 +15,7 @@ using TUGraz.VectoCore.Models.Simulation.DataBus;
 using TUGraz.VectoCore.Models.Simulation.Impl;
 using TUGraz.VectoCore.Models.SimulationComponent;
 using TUGraz.VectoCore.Models.SimulationComponent.Data;
+using TUGraz.VectoCore.Models.SimulationComponent.Data.ElectricComponents;
 using TUGraz.VectoCore.Models.SimulationComponent.Data.ElectricComponents.Battery;
 using TUGraz.VectoCore.Models.SimulationComponent.Impl;
 using TUGraz.VectoCore.OutputData;
@@ -408,6 +409,11 @@ public class DummyRunPowertrainComponentFactory : IPowertrainComponentFactory
     }
 
     public ITorqueSplitter CreateTorqueSplitter(IVehicleContainer container, IElectricPowerJunctionBox junctionBox)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IFuelCellSystem CreateFuelCellSystem(IVehicleContainer container, FuelCellSystemData modelData)
     {
         throw new NotImplementedException();
     }
